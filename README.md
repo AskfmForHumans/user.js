1fa9f5330ff307b7ab29d1bc1cb18ded753add86
snowwm <snowwontmelt@gmail.com>
2021-04-30 19:55:45 +0400

    Add themes.user.js

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 [![Greasy Fork](https://img.shields.io/badge/dynamic/json?color=success&label=Greasy%20Fork&query=%24.version&url=https%3A%2F%2Frunkit.io%2Fmysticflute%2Fuserscripts%2Fbranches%2Fmaster%2Fgreasyfork%2F422546)](https://greasyfork.org/ru/scripts/422546-askfmforhumans-1coin)
 
-Restore 1 click = 1 coin (not 5 coins) behavior. UI updates after the request to the backend is completed. Multiple clicks in a row also work correctly.
+Restore 1 click = 1 coin (not 5 coins) behavior.
+
+The UI updates after a short pause. Multiple clicks in a row also work correctly.
 
 ![Script demo GIF](imgs/1coin-1.gif)
+
+## themes.user.js
+
+[![Greasy Fork](https://img.shields.io/badge/dynamic/json?color=success&label=Greasy%20Fork&query=%24.version&url=https%3A%2F%2Frunkit.io%2Fmysticflute%2Fuserscripts%2Fbranches%2Fmaster%2Fgreasyfork%2F425738)](https://greasyfork.org/ru/scripts/425738-askfmforhumans-themes)
+
+Restore the choice from 18 color themes (selectable in the userscript manager menu).
+
+As ASKfm's style and layout are evolving, old CSS may not work smoothly / not work at all. Currently I've added a manual fix for red button borders. Help with adapting old styles for the new layout will be appreciated.
---
+++ themes.user.js
@@ -0,0 +1,72 @@
+// ==UserScript==
+// @name         AskfmForHumans/themes
+// @version      1.0
+// @namespace    https://github.com/AskfmForHumans
+// @author       https://github.com/AskfmForHumans
+// @homepage     https://github.com/AskfmForHumans/user.js
+// @license      MIT
+//
+// @description  Restore the choice from 18 color themes (selectable in the userscript manager menu)
+// @description:RU Возвращает выбор из 18 цветовых тем (управление через меню менеджера юзерскриптов)
+//
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
+// @grant        GM_registerMenuCommand
+// @grant        GM_unregisterMenuCommand
+// @match        https://ask.fm/*
+// @resource     style https://github.com/AskfmForHumans/user.js/raw/main/themes.css
+// ==/UserScript==
+
+(function () {
+    "use strict"
+
+    const THEME_NUMBERS = [...Array(18).keys()]
+    const MENU_ITEMS = []
+    const LOG_PREFIX = "AskfmForHumans/themes:"
+
+    GM_addStyle(GM_getResourceText("style"))
+    rebuildMenu()
+    applyTheme()
+
+    // Re-apply when a new body is created (on URL change).
+    const observer = new MutationObserver(applyTheme)
+    observer.observe(document.documentElement, {
+        childList: true,
+    })
+
+    function rebuildMenu() {
+        const currentTheme = GM_getValue("afh-askfm-theme", null)
+        for (const i of MENU_ITEMS) {
+            GM_unregisterMenuCommand(i)
+        }
+        MENU_ITEMS.length = 0
+        for (const i of THEME_NUMBERS) {
+            const id = `theme-${i}`
+            const name = (id === currentTheme ? `Theme ${i} ✔️` : `Theme ${i}`)
+            const cmd = GM_registerMenuCommand(name, toggleTheme.bind(null, id))
+            MENU_ITEMS.push(cmd)
+        }
+    }
+
+    function toggleTheme(id) {
+        const currentTheme = GM_getValue("afh-askfm-theme", null)
+        const newTheme = (id === currentTheme ? null : id)
+        console.info(LOG_PREFIX, `toggle old=${currentTheme} new=${newTheme}`)
+        GM_setValue("afh-askfm-theme", newTheme)
+        rebuildMenu()
+        applyTheme()
+    }
+
+    function applyTheme() {
+        const currentTheme = GM_getValue("afh-askfm-theme", null)
+        console.info(LOG_PREFIX, `apply ${currentTheme}`)
+        for (const i of THEME_NUMBERS) {
+            document.body.classList.remove(`theme-${i}`)
+        }
+        if (currentTheme !== null) {
+            document.body.classList.add(currentTheme)
+        }
+    }
+})()
